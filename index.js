86d40ac88da124b40044534e0bc1116afd62f9d3
Patricio <pabrisdev@gmail.com>
2023-04-13 21:43:32 -0500

    Actualizar el estado del TODO

--- index.js
+++ index.js
@@ -1,7 +1,63 @@
 const dom = document;
-// let TODOS = window.localStorage.getItem("todos");
+let TODOS = JSON.parse(window.localStorage.getItem("todoList"));
+const taskBox = dom.querySelector(".task-box");
 
-const clickHandler = (event) => {};
+const updateTodoStatus = (selectedTodo) => {
+  const todoName = selectedTodo.parentElement.lastElementChild;
+  if (selectedTodo.checked) {
+    todoName.classList.add("checked");
+    TODOS[selectedTodo.id].status = "completed";
+  } else {
+    todoName.classList.remove("checked");
+    TODOS[selectedTodo.id].status = "pending";
+  }
+  localStorage.setItem("todoList", JSON.stringify(TODOS));
+};
+
+const showMenu = (selectedTodo) => {
+  let todoMenu = selectedTodo.parentElement.lastElementChild;
+  todoMenu.classList.add("show");
+  document.onclick = ({ target }) => {
+    if (target.tagName !== "I" || target !== selectedTodo) {
+      todoMenu.classList.remove("show");
+    }
+  };
+};
+
+const clickHandler = ({ target }) => {
+  if (target.matches(".todo-selected")) updateTodoStatus(target);
+  if (target.matches(".settings i")) showMenu(target);
+};
+
+const showTodos = () => {
+  let li = "";
+
+  if (TODOS) {
+    TODOS.forEach(({ nameTodo, status }, id) => {
+      let isCompleted = status === "completed" ? "checked" : "";
+      // console.log(id, nameTodo, status);
+      li += `
+                <li class="task">
+                <label for="${id}">
+                    <input class="todo-selected" ${isCompleted} type="checkbox" id="${id}" />
+                    <p class="${isCompleted}">${nameTodo}</p>
+                </label>
+                <div class="settings">
+                    <i class="fa-solid fa-ellipsis"></i>
+                    <ul class="task-menu">
+                    <li><i class="fa-solid fa-pencil"></i> Edit</li>
+                    <li><i class="fa-solid fa-trash"></i> Delete</li>
+                    </ul>
+                </div>
+                </li>
+            
+            `;
+    });
+  }
+  taskBox.innerHTML = li;
+};
+
+showTodos();
 
 const keyUpHandler = (event) => {
   const taskName = dom.querySelector(".task-name-input input").value.trim();
