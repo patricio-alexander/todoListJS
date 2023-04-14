070db4f9d1767d22ca92c391623cffab09d41ec7
Patricio <pabrisdev@gmail.com>
2023-04-13 20:11:50 -0500

    Obtener el nombre del todo y guardarlo en localStorage

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const dom = document;
+// let TODOS = window.localStorage.getItem("todos");
 
-const load = (event ) => {
-    console.log(event);
-}
+const clickHandler = (event) => {};
 
+const keyUpHandler = (event) => {
+  const taskName = dom.querySelector(".task-name-input input").value.trim();
+  // console.log(taskName);
 
+  if (event.key === "Enter" && taskName) {
+    let TODOS = JSON.parse(window.localStorage.getItem("todoList"));
+    if (!TODOS) {
+      TODOS = [];
+    }
+    let taskInfo = { nameTodo: taskName, status: "pending" };
+    TODOS.push(taskInfo);
+    localStorage.setItem("todoList", JSON.stringify(TODOS));
+    console.log(TODOS);
+  }
+};
+
+const load = () => {
+  dom.addEventListener("click", clickHandler);
+  dom.addEventListener("keyup", keyUpHandler);
+};
 
 dom.addEventListener("DOMContentLoaded", load);
